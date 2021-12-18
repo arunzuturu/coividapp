fcadbb27a7d60cfaed234dcc9a6f92b3c51f88a3
Arun Adithya <78028212+arunzuturu@users.noreply.github.com>
2021-12-18 16:41:27 +0530

    intial

---
+++ README.md
@@ -0,0 +1,14 @@
+# COVID-19 Case Tracker
+
+An app to track corona case number and shows more info about Corona Virus
+
+## Getting Started
+
+A few resources to get you started if this is your first Flutter project:
+
+- [Lab: Write your first Flutter app](https://flutter.dev/docs/get-started/codelab)
+- [Cookbook: Useful Flutter samples](https://flutter.dev/docs/cookbook)
+
+For help getting started with Flutter, view our
+[online documentation](https://flutter.dev/docs), which offers tutorials,
+samples, guidance on mobile development, and a full API reference.
---
+++ lib/routes/symptoms.dart
@@ -0,0 +1,86 @@
+import 'package:covid/widget/text.dart';
+import 'package:flutter/material.dart';
+
+class Symptoms extends StatefulWidget {
+  Symptoms({Key? key}) : super(key: key);
+
+  @override
+  _SymptomsState createState() => _SymptomsState();
+}
+
+class _SymptomsState extends State<Symptoms> {
+  @override
+  Widget build(BuildContext context) {
+    var screenSize = MediaQuery.of(context).size;
+    var width = screenSize.width;
+    var height = screenSize.height;
+    return SafeArea(
+      child: Scaffold(
+        body: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                IconButton(
+                  onPressed: () {
+                    Navigator.popAndPushNamed(context, '/Home');
+                    //scaffoldKey.currentState!.openDrawer();
+                  },
+                  icon: Icon(
+                    Icons.arrow_back,
+                    color: Colors.black,
+                  ),
+                  iconSize: 24,
+                ),
+                TextMainNormal('Symptoms', 20),
+                Padding(padding: EdgeInsets.all(16)),
+              ],
+            ),
+            SizedBox(
+              height: height - 72,
+              child: LayoutBuilder(
+                builder:
+                    (BuildContext context, BoxConstraints viewportConstraints) {
+                  return SingleChildScrollView(
+                    child: Column(
+                      children: [
+                        Image(
+                          width: width,
+                          height: height * 0.40,
+                          fit: BoxFit.contain,
+                          image: NetworkImage(
+                              "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR-RaNVe7YVjIlyPh_aEul2Goqvqw5s2ncS9g&usqp=CAU"),
+                        ),
+                        Container(
+                          width: width,
+                          padding: EdgeInsets.all(20),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            children: [
+                              TextMain('Most common symptoms', 16),
+                              TextMainNormal(
+                                  '- fever\n- dry cough\n- tiredness\n', 14),
+                              TextMain('Less common symptoms', 16),
+                              TextMainNormal(
+                                  '- aches and pains\n- sore throat\n- diarrhoea\n- conjunctivitis\n- headache\n- loss of taste or smell\n- a rash on skin, or discolouration of fingers or toes\n',
+                                  14),
+                              TextMain('Serious common symptoms', 16),
+                              TextMainNormal(
+                                  '- difficulty breathing or shortness of breath\n- chest pain or pressure\n- loss of speech or movement',
+                                  14),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
